f4c8bbb1a4c71fd675cba8b134801c00aae1d1a1
pro <pro@prodeMacBook-Pro-2.local>
2023-08-29 00:14:36 +0600

    show data

--- first_app/models.py
+++ first_app/models.py
@@ -101,7 +101,7 @@ class Student(models.Model):
 
 
 class Teacher(models.Model):
-    student = models.ManyToManyField(Student)
+    student = models.ManyToManyField(Student, related_name='teachers')
     name = models.CharField(max_length=30)
     subject = models.CharField(max_length=20)
     mobile = models.CharField(max_length=15)
--- first_app/views.py
+++ first_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from first_app.forms import StudentForm
+from .models import Teacher, Student
 # Create your views here.
 
 
@@ -12,3 +13,17 @@ def home(request):
     else:
         form = StudentForm()
     return render(request, 'home.html', {'form': form})
+
+
+def showData(request):
+    # students list for one teacher
+    teacher = Teacher.objects.get(name='sobur')
+    students = teacher.student.all()
+    for stud in students:
+        print(stud.name)
+    # teacher list for one student
+    student = Student.objects.get(name='Tanver')
+    teachers = student.teachers.all()
+    for teacher in teachers:
+        print(teacher.name)
+    return render(request, 'show_data.html')
